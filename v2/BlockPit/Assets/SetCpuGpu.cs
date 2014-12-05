7cc78adbe469c0d9fd1f465d3c763a1c617b04a3
James Donald <jdonald@leapmotion.com>
2014-12-04 23:52:14 -0800

    Maximum CPU and downclock the GPU for tracking performance

---
+++ v2/BlockPit/Assets/SetCpuGpu.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.InteropServices;
+using UnityEngine;
+using System.Collections;
+
+public class SetCpuGpu : MonoBehaviour {
+	[DllImport("OculusPlugin")]
+	// Set the fixed CPU clock level.
+	private static extern void OVR_VrModeParms_SetCpuLevel( int cpuLevel );
+	
+	[DllImport("OculusPlugin")]
+	// Set the fixed GPU clock level.
+	private static extern void OVR_VrModeParms_SetGpuLevel( int gpuLevel );
+
+	void Start () {
+	}
+
+	void Awake () {
+#if UNITY_ANDROID && !UNITY_EDITOR
+		OVR_VrModeParms_SetCpuLevel( 3 );
+		OVR_VrModeParms_SetGpuLevel( 1 );
+#endif
+	}
+}
